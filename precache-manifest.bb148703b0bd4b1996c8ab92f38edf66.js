d30b9a91527dc3976e267316ca654274bd1f52e8
dmitrii_martynov <dmitriimartynov1@gmail.com>
2020-10-07 11:08:19 +0300

    Updates

---
+++ precache-manifest.bb148703b0bd4b1996c8ab92f38edf66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea8e432a656beecb2cf9aede84ec78e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fa1f655a0f5efdc932a8",
+    "url": "/monsters-rolodex/static/css/main.bc249a22.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1f655a0f5efdc932a8",
+    "url": "/monsters-rolodex/static/js/main.7bf9debb.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
